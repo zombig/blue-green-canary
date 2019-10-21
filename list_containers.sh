8d219fda6b8349e99a5f99c5d14e71045a805d01
Burr Sutter <burrsutter@gmail.com>
2019-10-21 16:48:11 -0400

    adding more scripts

--- list_containers.sh
+++ list_containers.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo 'Containers'
+
 kubectl get pod $1 -o yaml | yq r - spec.containers[*].name
+
+echo 'Init Containers'
+
+kubectl get pod $1 -o yaml | yq r - spec.initContainers[*].name
