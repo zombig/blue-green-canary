2c42a47aad47f80cd8133745edaf2340b62c07eb
Burr Sutter <burrsutter@gmail.com>
2019-10-22 18:15:32 -0400

    green working, attempting blue/canary

--- start_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tkn pipeline start bgc-build-deploy \
- --param="mavenMirrorUrl=http://nexus.rhd-workshop-infra:8081/nexus/content/groups/public"  \
- --resource="appSource=bgc-git-source" \
- --resource="appImage=bgc-image" \
- --serviceaccount="pipeline"
--- start_pipeline_nobuild.sh
+++ start_pipeline_nobuild.sh
@@ -2,6 +2,7 @@
 
 tkn pipeline start bgc-skip-build-deploy \
  --param="mavenMirrorUrl=http://nexus.rhd-workshop-infra:8081/nexus/content/groups/public"  \
+ --param="kubernetesResourceFile=/workspace/source/src/main/kubernetes/Deployment_green.yml" \
  --resource="appSource=bgc-git-source" \
  --resource="appImage=bgc-image" \
  --serviceaccount="pipeline"
