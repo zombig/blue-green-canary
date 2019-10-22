5a2e4f0d2f20a66f843a54215df2d9304bfb5311
Burr Sutter <burrsutter@gmail.com>
2019-10-22 18:32:08 -0400

    getting all scripts in

---
+++ setup_pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+oc create -f src/main/tekton/pipeline-resources-openshift-green.yml
+oc apply -f src/main/tekton/task-yq-deploy.yml
+oc apply -f src/main/tekton/task-mvn-buildah.yml
+oc apply -f src/main/tekton/pipeline.yml
+
+# oc replace -f src/main/tekton/pipeline-resources-openshift-green.yml
+# oc replace -f src/main/tekton/task-yq-deploy.yml
+# oc replace -f src/main/tekton/task-mvn-buildah.yml
+# oc replace -f src/main/tekton/pipeline-skip-build.yml
---
+++ start_pipeline_green.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tkn pipeline start bgc-build-deploy \
+ --param="mavenMirrorUrl=http://nexus.rhd-workshop-infra:8081/nexus/content/groups/public"  \
+ --param="kubernetesResourceFile=/workspace/source/src/main/kubernetes/Deployment_green.yml" \
+ --resource="appSource=bgc-git-source" \
+ --resource="appImage=bgc-image" \
+ --serviceaccount="pipeline"
