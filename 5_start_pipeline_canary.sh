67edd00a1fdd0bbc21b873dd5f3ef11f3e34cc7d
Burr Sutter <burrsutter@gmail.com>
2019-10-27 22:00:02 +0300

    might have blue vs green vs canary separated

--- 5_start_pipeline_canary.sh
+++ 5_start_pipeline_canary.sh
@@ -4,5 +4,5 @@ tkn pipeline start bgc-build-deploy \
  --param="mavenMirrorUrl=http://nexus.rhd-workshop-infra:8081/nexus/content/groups/public"  \
  --param="kubernetesResourceFile=/workspace/source/src/main/kubernetes/Deployment_canary.yml" \
  --resource="appSource=bgc-git-source" \
- --resource="appImage=bgc-image" \
+ --resource="appImage=bgc-image-canary" \
  --serviceaccount="pipeline"
--- 5_start_pipeline_green.sh
+++ 5_start_pipeline_green.sh
@@ -4,5 +4,5 @@ tkn pipeline start bgc-build-deploy \
  --param="mavenMirrorUrl=http://nexus.rhd-workshop-infra:8081/nexus/content/groups/public"  \
  --param="kubernetesResourceFile=/workspace/source/src/main/kubernetes/Deployment_green.yml" \
  --resource="appSource=bgc-git-source" \
- --resource="appImage=bgc-image" \
+ --resource="appImage=bgc-image-green" \
  --serviceaccount="pipeline"
